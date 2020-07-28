b251a741d7c17d985b4cbc6be71ad1677faa64ef
Devesh Kumar Sharma <deveshkumarsharma475@gmail.com>
2020-07-28 15:32:30 +0530

    'final'

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '2lis@ze($0*%m9gy9oxfm2k3!h-*)0jlp&)nu0wpasj+e1grr2'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['localhost','https://blog4friends.herokuapp.com/']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -76,11 +77,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.path.join(BASE_DIR, 'dcnst20c38l20c'),
+        'HOST': 'ec2-54-234-28-165.compute-1.amazonaws.com',
+        'PORT':5432,
+        'USER':'tbczhsbojqelpd',
+        'PASSWORD' : 'e8551cfb701ff178099374ba190dc4b42d3932daa7260774688ac237e3e64d87'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -119,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'blog/static')
+
 
 LOGIN_REDIRECT_URL = '/'
